0baeb459f9bcf8ba02c3862cc62cd7d1886a66c4
chris sidi <hashtagchris@github.com>
2020-01-09 21:30:03 -0500

    Add create-issues.js

---
+++ create-issues.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const Octokit = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: process.env["GITHUB_PAT"],
+});
+
+(async function() {
+  // await octokit.issues.create({
+  //   owner: 'hashtagchris',
+  //   repo: 'test-workflows',
+  //   title: `foo-bar-baz`,
+  // });
+
+  for (let i = 0; i < 4; i++) {
+    for (let letter = 97; letter < 123; letter++) {
+      const title = `${Buffer.from([letter]).toString()}-${i}`;
+
+      console.log(`Creating issue. title: ${title}`);
+      await octokit.issues.create({
+        owner: 'hashtagchris',
+        repo: 'test-workflows',
+        title,
+      });
+    }
+  }
+})();
