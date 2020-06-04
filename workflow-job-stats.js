e6af0edb2d3aac37e18abe37fe6c92d7980dbde4
Chris Sidi <hashtagchris@github.com>
2020-06-03 23:16:11 -0400

    Use workflow names in table.

--- workflow-job-stats.js
+++ workflow-job-stats.js
@@ -2,7 +2,6 @@
 // GITHUB_PAT is optional for public repos, but gives you higher rate limits.
 
 // Enhancements:
-// * Resolve workflow_id to a name.
 // * Accept a specific workflow_id / name as an argument.
 // * Accept a page limit or a cutoff date.
 
@@ -18,8 +17,44 @@ if (process.argv.length !== 3) {
 
 const nwo = process.argv[2];
 console.log(`owner/repository: ${nwo}`);
+console.log();
 
-async function getWorkflowRuns(nwo) {
+async function getWorkflows(nwo) {
+  const args = {
+    headers: {
+        "User-Agent": "hashtagchris-github-util/0.1"
+    },
+    json: true,
+    transform: function (body, response, resolveWithFullResponse) {
+        // Trace the pagination header for debugging.
+        // console.log(response.headers.link);
+
+        // Don't modify the body.
+        return body;
+    }
+  };
+
+  if (process.env.GITHUB_PAT) {
+    args.headers.Authorization = `Bearer ${process.env["GITHUB_PAT"]}`;
+  }
+
+  let workflows = [];
+  for (let page = 1; page < 9999; page++) {
+      const url = `https://api.github.com/repos/${nwo}/actions/workflows?page=${page}`;
+      // console.log(url);
+      const results = await request.get(url, args);
+
+      if (results.workflows.length == 0) {
+        break;
+      }
+
+      workflows.push(...results.workflows);
+  }
+
+  return workflows;
+}
+
+async function getRunsForWorkflow(nwo, workflowId) {
   const args = {
     headers: {
         "User-Agent": "hashtagchris-github-util/0.1"
@@ -40,7 +75,7 @@ async function getWorkflowRuns(nwo) {
 
   let workflowRuns = [];
   for (let page = 1; page < 9999; page++) {
-      const url = `https://api.github.com/repos/${nwo}/actions/runs?page=${page}`;
+      const url = `https://api.github.com/repos/${nwo}/actions/workflows/${workflowId}/runs?page=${page}`;
       // console.log(url);
       const results = await request.get(url, args);
 
@@ -53,6 +88,41 @@ async function getWorkflowRuns(nwo) {
 
   return workflowRuns;
 }
+
+// async function getWorkflowRuns(nwo) {
+//   const args = {
+//     headers: {
+//         "User-Agent": "hashtagchris-github-util/0.1"
+//     },
+//     json: true,
+//     transform: function (body, response, resolveWithFullResponse) {
+//         // Trace the pagination header for debugging.
+//         // console.log(response.headers.link);
+
+//         // Don't modify the body.
+//         return body;
+//     }
+//   };
+
+//   if (process.env.GITHUB_PAT) {
+//     args.headers.Authorization = `Bearer ${process.env["GITHUB_PAT"]}`;
+//   }
+
+//   let workflowRuns = [];
+//   for (let page = 1; page < 9999; page++) {
+//       const url = `https://api.github.com/repos/${nwo}/actions/runs?page=${page}`;
+//       // console.log(url);
+//       const results = await request.get(url, args);
+
+//       if (results.workflow_runs.length == 0) {
+//         break;
+//       }
+
+//       workflowRuns.push(...results.workflow_runs);
+//   }
+
+//   return workflowRuns;
+// }
 
 async function getJobsForWorkflowRun(nwo, runId) {
   const args = {
@@ -83,41 +153,55 @@ async function getJobsForWorkflowRun(nwo, runId) {
 }
 
 (async function () {
-  const workflowRuns = await getWorkflowRuns(nwo);
-
-  if (workflowRuns.length === 0) {
-    throw new Error(`No workflow runs found for ${nwo}.`);
+  const workflows = await getWorkflows(nwo);
+  if (workflows.length === 0) {
+    console.error(`No workflows found for ${nwo}.`);
+    return;
   }
 
-  // console.log(workflowRuns);
-
   const jobStats = {};
-  for (const workflowRun of workflowRuns) {
-    const jobs = await getJobsForWorkflowRun(nwo, workflowRun.id);
-    for (const job of jobs) {
-      if (!job.completed_at) {
-        if (job.status === "completed") {
-          console.error(`Skipping completed job with completed_at set to ${job.completed_at}`);
-        }
-        continue;
-      }
-      const key = `${job.name} (${workflowRun.workflow_id})`;
-      if (jobStats[key] === undefined) {
-        jobStats[key] = {
-          executionCount: 0,
-          sumElapsedTimeMs: 0,
-          elapsedTimesMs: [],
-        };
-      }
 
-      const elapsedTimeMs = new Date(job.completed_at) - new Date(job.started_at);
+  for (const workflow of workflows) {
+    const workflowRuns = await getRunsForWorkflow(nwo, workflow.id);
 
-      const js = jobStats[key];
-
-      js.executionCount++;
-      js.sumElapsedTimeMs += elapsedTimeMs;
-      js.elapsedTimesMs.push(elapsedTimeMs);
+    if (workflowRuns.length === 0) {
+      continue;
     }
+
+    // console.log(workflowRuns);
+
+    for (const workflowRun of workflowRuns) {
+      const jobs = await getJobsForWorkflowRun(nwo, workflowRun.id);
+      for (const job of jobs) {
+        if (!job.completed_at) {
+          if (job.status === "completed") {
+            console.error(`Skipping completed job with completed_at set to ${job.completed_at}`);
+          }
+          continue;
+        }
+        const key = `${workflow.name} / ${job.name}`;
+        if (jobStats[key] === undefined) {
+          jobStats[key] = {
+            executionCount: 0,
+            sumElapsedTimeMs: 0,
+            elapsedTimesMs: [],
+          };
+        }
+
+        const elapsedTimeMs = new Date(job.completed_at) - new Date(job.started_at);
+
+        const js = jobStats[key];
+
+        js.executionCount++;
+        js.sumElapsedTimeMs += elapsedTimeMs;
+        js.elapsedTimesMs.push(elapsedTimeMs);
+      }
+    }
+  }
+
+  if (Object.keys(jobStats) === 0) {
+    console.log(`No workflow runs found for '${nwo}' repo.`);
+    return;
   }
 
   // console.log(jobStats);
@@ -140,10 +224,13 @@ async function getJobsForWorkflowRun(nwo, runId) {
   }
 
   // sort table by execution count desc.
-  tableData.sort((a, b) => b[1] - a[1]);
+  // tableData.sort((a, b) => b[1] - a[1]);
+
+  // sort table by workflow / job name.
+  tableData.sort((a, b) => a[0] > b[0]);
 
   // Prefix the header row.
-  tableData.unshift(["Job name (workflow_id)", "Execution count", "Total elapsed seconds", "Elapsed seconds (median)", "Elapsed seconds (99p)"]);
+  tableData.unshift(["Workflow name / Job name", "Execution count", "Total elapsed seconds", "Elapsed seconds (median)", "Elapsed seconds (99p)"]);
 
   const tableConfig = {
     columns: {
