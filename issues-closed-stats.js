a6ab30a0e247eb76f3b6fa9df5bc3f1d6e2ea0b1
Chris Sidi <hashtagchris@github.com>
2021-07-15 00:46:38 -0400

    Add issues-closed-stats.js

---
+++ issues-closed-stats.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env["GITHUB_PAT"],
+});
+
+(async function() {
+  const labels = ['incident-repair']
+  const oneDayMS = 24 * 60 * 60 * 1000
+  const weeklyClosedIssues = new Object()
+
+  for (let page = 1; page < 10; page++) {
+    const issuesResponse = await octokit.issues.listForRepo({
+      owner: 'github',
+      repo: 'c2c-actions-experience',
+      labels,
+      state: 'closed',
+      page,
+    });
+
+    if (issuesResponse.data.length === 0) {
+      break
+    }
+
+    for (const issue of issuesResponse.data) {
+      if (issue.labels.some(l => l.name == 'Closed by stale')) {
+        console.log(`closed by stale: ${issue.number}`)
+        continue
+      }
+
+      const closedDate = new Date(issue.closed_at)
+      let closedWeek = new Date(closedDate.getFullYear(), closedDate.getMonth(), closedDate.getDate())
+      closedWeek = closedWeek.valueOf() - (closedWeek.getDay() * oneDayMS) + oneDayMS
+
+      if (weeklyClosedIssues[closedWeek] === undefined) {
+        weeklyClosedIssues[closedWeek] = []
+      }
+
+      weeklyClosedIssues[closedWeek].push(issue.number)
+    }
+  }
+
+  console.log(`Labels: ${labels}`)
+  console.log()
+
+  for (const week of Object.keys(weeklyClosedIssues).sort((a, b) => { return b - a})) {
+    const weekDate = new Date(Number.parseInt(week))
+    const weeklyIssues = weeklyClosedIssues[week].sort()
+    console.log(`${weekDate.toDateString()}: ${weeklyIssues.length} issues closed (${weeklyIssues.join(', ')})`)
+  }
+})();
